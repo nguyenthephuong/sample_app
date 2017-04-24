21a5335f3baa145f0d37f1adcdbdc6c12bd0d795
phuongnt <thephuong1995@gmail.com>
2017-04-24 11:43:55 +0700

    Implement basic login 3th

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = t "layouts.static_pages.home.welcome"
+      log_in @user
+      flash[:success] = t "welcome"
       redirect_to @user
     else
       render :new
