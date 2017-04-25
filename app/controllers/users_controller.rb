7a3ff43a1a3f36bb1a4e9c17a1f4ac20fe0547bb
phuongnt <thephuong1995@gmail.com>
2017-04-25 15:09:05 +0700

    Finish user edit, update, index, and destroy actions 5th

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  before_action :reject_if_logged_in, only: [:new, :create]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :find_user, only: [:show, :edit, :update, :destroy,
+    :correct_user]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page], per_page: 8
+  end
+
   def new
     @user = User.new
   end
@@ -7,7 +18,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       log_in @user
-      flash[:success] = t "welcome"
+      flash[:success] = t ".success"
       redirect_to @user
     else
       render :new
@@ -15,16 +26,53 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by id: params[:id]
-    unless @user
-      flash[:danger] = t ".danger"
-      redirect_to signup_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      render :edit
     end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = t ".success"
+    redirect_to users_path
   end
 
   private
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "users_dander"
+      redirect_to login_path
+    end
+  end
+
+  def correct_user
+    redirect_to root_path unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:danger] = t ".danger"
+      redirect_to root_path
+    end
   end
 end
