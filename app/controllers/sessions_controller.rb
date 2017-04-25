9e5e9d9e3c1bdeb3e2b0428e51ad58acaa683d4a
phuongnt <thephuong1995@gmail.com>
2017-04-25 15:09:05 +0700

    Finish user edit, update, index, and destroy actions 5th

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :reject_if_logged_in, only: [:new, :create]
+
   def new
   end
 
@@ -6,15 +8,16 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+      redirect_back_or user
     else
-      flash.now[:danger] = t "danger_login"
+      flash.now[:danger] = t "session_danger"
       render :new
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
