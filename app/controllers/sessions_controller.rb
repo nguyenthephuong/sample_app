099aa85a0569bc5a906b945a68c5ed1932963502
phuongnt <thephuong1995@gmail.com>
2017-05-10 11:00:15 +0700

    Add account activation 4th

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        flash[:warning] = t "message_active"
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t "session_danger"
       render :new
